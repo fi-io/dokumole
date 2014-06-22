15def01944a6e456fdf2fb52e79d1b0e0df51407
Brij Mohan Lal Srivastava <contactbrijmohan@gmail.com>
2014-06-22 05:37:06 +0530

    Created dynamic grid using angular directive

--- javascripts/dokumole.js
+++ javascripts/dokumole.js
@@ -1,50 +1,33 @@
-var dokumoleApp = angular.module('dokumoleApp', ["ngSanitize"]);
+var dokumoleApp = angular.module('dokumoleApp', []);
+dokumoleApp.filter('range', function() {
+  return function(input, total) {
+    total = parseInt(total);
+    for (var i=0; i<total; i++)
+      input.push(i);
+    return input;
+  };
+});
+dokumoleApp.directive('sudokuGrid', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/sudoku-grid.html'
+	};
+});
 function MainController($scope) {
 	$scope.nvalue = 0;
 	$scope.grid = "";
 	$scope.$cellMatrix = {};
 	$scope.setNvalue = function(n) {
 		$scope.nvalue = n;
-		$scope.grid = generateGrid();
-		//console.log($scope.grid);
-		$('.sudoku').html($scope.grid);
 	}
-	function generateGrid() {
+	$scope.gridCellClass = function(i, j) {
 		var nsqrt = Math.sqrt($scope.nvalue);
-		var $td, $tr,
-			$table = $( '<table>' )
-				.addClass( 'sudoku-container' );
-
-		for ( var i = 0; i < $scope.nvalue; i++ ) {
-			$tr = $( '<tr>' );
-			$scope.$cellMatrix[i] = {};
-
-			for ( var j = 0; j < $scope.nvalue; j++ ) {
-				// Build the input
-				$scope.$cellMatrix[i][j] = $( '<input>' )
-					.attr( 'maxlength', 1 )
-					.attr( 'type', 'text' )
-					.data( 'row', i )
-					.data( 'col', j )
-
-				$td = $( '<td>' ).append( $scope.$cellMatrix[i][j] );
-				// Calculate section ID
-				sectIDi = Math.floor( i / nsqrt );
-				sectIDj = Math.floor( j / nsqrt );
-				// Set the design for different sections
-				if ( ( sectIDi + sectIDj ) % 2 === 0 ) {
-					$td.addClass( 'sudoku-section-one' );
-				} else {
-					$td.addClass( 'sudoku-section-two' );
-				}
-				// Build the row
-				$tr.append( $td );
-			}
-			// Append to table
-			$table.append( $tr );
+		var sectIDi = Math.floor( i / nsqrt );
+		var sectIDj = Math.floor( j / nsqrt );
+		if ( ( sectIDi + sectIDj ) % 2 === 0 ) {
+			return 'sudoku-section-one';
+		} else {
+			return 'sudoku-section-two';
 		}
-		// Return the GUI table
-		//console.log($table);
-		return $table;
 	}
 };
