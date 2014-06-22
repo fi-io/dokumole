555b3c78d87e2a60e85a495e69c2da6b2102f6f7
Brij Mohan Lal Srivastava <contactbrijmohan@gmail.com>
2014-06-22 08:01:02 +0530

    Completed basic working of sudoku solver

--- javascripts/dokumole.js
+++ javascripts/dokumole.js
@@ -10,24 +10,97 @@ dokumoleApp.filter('range', function() {
 dokumoleApp.directive('sudokuGrid', function() {
 	return {
 		restrict: 'E',
-		templateUrl: 'templates/sudoku-grid.html'
+		templateUrl: 'templates/sudoku-grid.html',
+		link: function(scope, element, attrs) {
+			element.on('keyup', function(evt) {
+				var str = "";
+				var inp_ele = element.find('input');
+				for (var i = 0; i < inp_ele.length; i++) {
+					if (!inp_ele[i].value) {
+						str += '0';
+					} else {
+						str += inp_ele[i].value;
+					}
+				}
+				scope.gridString = str;
+				scope.$digest();
+			});
+			scope.$watch('result', function(newval) {
+				var inp_ele = element.find('input');
+				for (var i = 0; i < inp_ele.length; i++) {
+					inp_ele[i].value = newval[i];
+				}
+			});
+		}
 	};
 });
 function MainController($scope) {
 	$scope.nvalue = 0;
+	$scope.nsqrt = 0;
+	$scope.nsq = 0;
+	$scope.possible_vals = [];
 	$scope.grid = "";
 	$scope.$cellMatrix = {};
 	$scope.setNvalue = function(n) {
 		$scope.nvalue = n;
+		$scope.nsqrt = Math.sqrt(n);
+		$scope.nsq = n * n;
+		for (var i = 1; i <= n; i++){
+			$scope.possible_vals.push(i.toString());
+		}
 	}
 	$scope.gridCellClass = function(i, j) {
-		var nsqrt = Math.sqrt($scope.nvalue);
-		var sectIDi = Math.floor( i / nsqrt );
-		var sectIDj = Math.floor( j / nsqrt );
+		var sectIDi = Math.floor( i / $scope.nsqrt );
+		var sectIDj = Math.floor( j / $scope.nsqrt );
 		if ( ( sectIDi + sectIDj ) % 2 === 0 ) {
-			return 'sudoku-section-one';
+			return 'bg-success';
 		} else {
 			return 'sudoku-section-two';
 		}
 	}
+	
+	$scope.solveSudoku = function() {
+		solve($scope.gridString);
+	}
+	
+	function same_row(i, j){
+		return (Math.floor(i/$scope.nvalue) == Math.floor(j/$scope.nvalue));
+	}
+	function same_col(i, j){
+		return ((i - j) % $scope.nvalue == 0);
+	}
+	function same_block(i, j){
+		return (Math.floor(i/($scope.nvalue * $scope.nsqrt)) == Math.floor(j/($scope.nvalue * $scope.nsqrt)) && Math.floor(i % $scope.nvalue / $scope.nsqrt) == Math.floor(j % $scope.nvalue / $scope.nsqrt));
+	}
+	function solve(input) {	
+		var i = input.indexOf('0');
+		
+		if (i == -1) {
+			// already solved
+			$scope.result = input;
+			throw "solved";
+		}
+		var excluded = [];
+		for (var j = 0; j < $scope.nsq; j++) {
+			if (same_row(i, j) || same_col(i, j) || same_block(i, j)) {
+				excluded.push(input[j]);
+			}
+		}
+		excluded = set(excluded);
+		for (m in $scope.possible_vals){
+			if (excluded.indexOf($scope.possible_vals[m]) == -1) {
+				solve(input.substr(0, i) + $scope.possible_vals[m] + input.substr(i + 1));
+			}
+		}
+	}
+
+	function set (arr) {
+		return arr.reduce(function (a, val) {
+			if (a.indexOf(val) === -1) {
+				a.push(val);
+			}
+			return a;
+		}, []);
+	}
+	
 };
