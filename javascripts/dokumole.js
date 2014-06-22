d44b96372ce80ee993877e347cbf20462d524ca0
Brij Mohan Lal Srivastava <contactbrijmohan@gmail.com>
2014-06-23 01:22:36 +0530

    Changed logic of sudoku solver

--- javascripts/dokumole.js
+++ javascripts/dokumole.js
@@ -1,3 +1,4 @@
+"use strict";
 var dokumoleApp = angular.module('dokumoleApp', []);
 dokumoleApp.filter('range', function() {
   return function(input, total) {
@@ -8,6 +9,7 @@ dokumoleApp.filter('range', function() {
   };
 });
 dokumoleApp.directive('sudokuGrid', function() {
+	"use strict";
 	return {
 		restrict: 'E',
 		templateUrl: 'templates/sudoku-grid.html',
@@ -45,6 +47,7 @@ dokumoleApp.directive('sudokuGrid', function() {
 	};
 });
 function MainController($scope) {
+	"use strict";
 	$scope.nvalue = 0;
 	$scope.nsqrt = 0;
 	$scope.nsq = 0;
@@ -56,6 +59,7 @@ function MainController($scope) {
 		$scope.nvalue = n;
 		$scope.nsqrt = Math.sqrt(n);
 		$scope.nsq = n * n;
+		$scope.rowCells = $scope.nvalue * $scope.nsqrt;
 		for (var i = 1; i <= n; i++){
 			$scope.possible_vals.push(i.toString());
 		}
@@ -75,41 +79,71 @@ function MainController($scope) {
 	}
 	
 	$scope.solveSudoku = function() {
-		solve($scope.gridString);
+		$scope.result = solve($scope.gridString);
 	}
 	
-	function same_row(i, j){
-		return (Math.floor(i/$scope.nvalue) == Math.floor(j/$scope.nvalue));
+	/* Function to get all row indexes of current element */
+	function getRowIndexes(i) {
+		"use strict";
+		var rowIndex = Math.floor(i / $scope.nvalue);
+		var list = [];
+		for (var i = rowIndex*$scope.nvalue; i < (rowIndex*$scope.nvalue + $scope.nvalue); i++) {
+			list.push(i);
+		}
+		return list;
 	}
-	function same_col(i, j){
-		return ((i - j) % $scope.nvalue == 0);
+	/* Function to get all col indexes of current element */
+	function getColIndexes(i) {
+		"use strict";
+		var colIndex = i % $scope.nvalue;
+		var list = [];
+		for (var i = colIndex; i < $scope.nsq; i += $scope.nvalue) {
+			list.push(i);
+		}
+		return list;
 	}
-	function same_block(i, j){
-		return (Math.floor(i/($scope.nvalue * $scope.nsqrt)) == Math.floor(j/($scope.nvalue * $scope.nsqrt)) && Math.floor(i % $scope.nvalue / $scope.nsqrt) == Math.floor(j % $scope.nvalue / $scope.nsqrt));
+	/* Function to get all col indexes of current element */
+	function getBlockIndexes(i) {
+		"use strict";
+		var block_row = Math.floor(i / $scope.rowCells);
+		var block_col = Math.floor(i % $scope.nvalue / $scope.nsqrt);
+		var f_num = (block_row * $scope.rowCells) + (block_col * $scope.nsqrt);
+		var list = [];
+		for (var i = f_num; i < (f_num + $scope.nsqrt); i++) {
+			for (var j = i, count = 0; count < $scope.nsqrt; count++, j+=$scope.nvalue) {
+				list.push(j);
+			}
+		}
+		return list;
 	}
-	function solve(input) {	
+	function solve(input) {
+		"use strict";
 		var i = input.indexOf('0');
 		
 		if (i == -1) {
 			// already solved
-			$scope.result = input;
-			throw "This is not an error. I need to fix this one. Dont worry about it. :)";
+			return input;
 		}
 		var excluded = [];
-		for (var j = 0; j < $scope.nsq; j++) {
-			if (same_row(i, j) || same_col(i, j) || same_block(i, j)) {
-				excluded.push(input[j]);
-			}
+		var excluded_indexes = getRowIndexes(i).concat(getColIndexes(i)).concat(getBlockIndexes(i));
+		for (var j = 0; j < excluded_indexes.length; j++) {
+			excluded.push(input[excluded_indexes[j]]);
 		}
-		excluded = set(excluded);
-		for (m in $scope.possible_vals){
+		//excluded = set(excluded);
+		for (var m in $scope.possible_vals){
 			if (excluded.indexOf($scope.possible_vals[m]) == -1) {
-				solve(input.substr(0, i) + $scope.possible_vals[m] + input.substr(i + 1));
+				var temp = solve(input.substr(0, i) + $scope.possible_vals[m] + input.substr(i + 1));
+				if(!temp) {
+					excluded.push($scope.possible_vals[m]);
+				} else {
+					return temp;
+				}
 			}
 		}
 	}
 
 	function set (arr) {
+		"use strict";
 		return arr.reduce(function (a, val) {
 			if (a.indexOf(val) === -1) {
 				a.push(val);
